d95eff3d28c3e6c6ac35a874930ab6502796453b
igomezv <neorelativista@gmail.com>
2021-04-20 03:05:33 -0500

    add paths to outputs and files

--- neuralike/NeuralManager.py
+++ neuralike/NeuralManager.py
@@ -4,37 +4,42 @@ import os
 
 
 class NeuralManager:
-    def __init__(self, loglikelihood, pars_bounds, rootname, ndivsgrid=50, hidden_layers_neurons=None,
-                 epochs=100, **kwargs):
+    def __init__(self, loglikelihood, pars_bounds, rootname, ndivsgrid=5, hidden_layers_neurons=None,
+                 epochs=100, plot=True, **kwargs):
         if hidden_layers_neurons is None:
             hidden_layers_neurons = [100, 100, 100]
         self.loglikelihood = loglikelihood
         self.pars_bounds = pars_bounds
         self.ndivsgrid = ndivsgrid
         self.epochs = epochs
+        self.plot = plot
+        self.grid_path = 'simplemc/analyzers/neuralike/neural_models/{}'.format(rootname)
         self.model_path = 'simplemc/analyzers/neuralike/neural_models/{}.h5'.format(rootname)
+        self.fig_path = 'simplemc/analyzers/neuralike/neural_models/{}.png'.format(rootname)
 
-        self.learning_rate = kwargs.pop('learning_rate', 1e-4)
-        self.batch_size = kwargs.pop('batch_size', 4)
+        self.learning_rate = kwargs.pop('learning_rate', 5e-4)
+        self.batch_size = kwargs.pop('batch_size', 32)
         self.early_tol = kwargs.pop('early_tol', 100)
         self.psplit = kwargs.pop('psplit', 0.8)
         self.topology = [len(pars_bounds)] + hidden_layers_neurons + [1]
 
         if not self.modelChecker():
-            self.neural_model = self.training()
-        else:
-            self.neural_model = self.load()
+            self.training()
+        self.neural_model = self.load()
+
 
     def training(self):
-        grid = GridLikes(self.loglikelihood, self.pars_bounds, ndivs=self.ndivsgrid)
+        grid = GridLikes(self.loglikelihood, self.pars_bounds, ndivs=self.ndivsgrid, files_path=self.grid_path)
         samples, likes = grid.make_dataset()
         neural_model = NeuralNet(X=samples, Y=likes, topology=self.topology, epochs=self.epochs,
                                  batch_size=self.batch_size, learrning_rate=self.learning_rate)
 
         neural_model.train()
         neural_model.save_model('{}'.format(self.model_path))
+        if self.plot:
+            neural_model.plot(save=True, figname='{}'.format(self.fig_path))
 
-        return neural_model
+        return True
 
     def load(self):
         neural_model = NeuralNet(load=True, model_path=self.model_path)
