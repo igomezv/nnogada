fe7e4c1dd3c15ccd2bd1e1777884b84b1d2228b3
igomezv <neorelativista@gmail.com>
2022-08-05 20:46:00 -0500

    move elitism within the class

--- neurapprox/Neurapprox.py
+++ neurapprox/Neurapprox.py
@@ -1,5 +1,4 @@
 from deap import base, creator, tools, algorithms
-from elitism import eaSimpleWithElitism
 import numpy as np
 from scipy.stats import bernoulli
 class Neurapprox:
@@ -53,7 +52,7 @@ class Neurapprox:
         hof = tools.HallOfFame(HALL_OF_FAME_SIZE)
 
         # Genetic Algorithm flow with elitism:
-        population, logbook = eaSimpleWithElitism(population, toolbox, cxpb=P_CROSSOVER, mutpb=P_MUTATION,
+        population, logbook = self.eaSimpleWithElitism(population, toolbox, cxpb=P_CROSSOVER, mutpb=P_MUTATION,
                                                   ngen=max_generations, stats=stats, halloffame=hof, verbose=True)
 
         # print info for best solution found:
@@ -124,3 +123,59 @@ class Neurapprox:
         self.history.append([deep_layers, num_units, learning_rate, loss, score, t])
 
         return loss,
+
+    def eaSimpleWithElitism(self, population, toolbox, cxpb, mutpb, ngen, stats=None,
+                            halloffame=None, verbose=__debug__):
+        """This algorithm is similar to DEAP eaSimple() algorithm, with the modification that
+        halloffame is used to implement an elitism mechanism. The individuals contained in the
+        halloffame are directly injected into the next generation and are not subject to the
+        genetic operators of selection, crossover and mutation.
+        """
+        logbook = tools.Logbook()
+        logbook.header = ['gen', 'nevals'] + (stats.fields if stats else [])
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in population if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        if halloffame is None:
+            raise ValueError("halloffame parameter must not be empty!")
+
+        halloffame.update(population)
+        hof_size = len(halloffame.items) if halloffame.items else 0
+
+        record = stats.compile(population) if stats else {}
+        logbook.record(gen=0, nevals=len(invalid_ind), **record)
+        if verbose:
+            print(logbook.stream)
+
+        # Begin the generational process
+        for gen in range(1, ngen + 1):
+
+            # Select the next generation individuals
+            offspring = toolbox.select(population, len(population) - hof_size)
+
+            # Vary the pool of individuals
+            offspring = algorithms.varAnd(offspring, toolbox, cxpb, mutpb)
+
+            # Evaluate the individuals with an invalid fitness
+            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+            fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+            for ind, fit in zip(invalid_ind, fitnesses):
+                ind.fitness.values = fit
+
+            # add the best back to population:
+            offspring.extend(halloffame.items)
+            # Update the hall of fame with the generated individuals
+            halloffame.update(offspring)
+            # Replace the current population by the offspring
+            population[:] = offspring
+            # Append the current generation statistics to the logbook
+            record = stats.compile(population) if stats else {}
+            logbook.record(gen=gen, nevals=len(invalid_ind), **record)
+            if verbose:
+                print(logbook.stream)
+
+        return population, logbook
