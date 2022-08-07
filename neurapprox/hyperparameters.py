99d07aef6c981b99caa64fdcefc393bfdc538b83
igomezv <neorelativista@gmail.com>
2022-08-06 23:47:21 -0500

    fix import

--- neurapprox/hyperparameters.py
+++ neurapprox/hyperparameters.py
@@ -1,4 +1,4 @@
-from Hyperparameter import Hyperparameter
+from neurapprox.Hyperparameter import Hyperparameter
 import numpy as np
 
 deep = Hyperparameter(name='deep', values=np.array([2, 3, 4]), val=3)
