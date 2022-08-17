73dc7525d21de3b5264d2a433c6bfd256203c4c0
igomezv <neorelativista@gmail.com>
2022-08-17 07:31:31 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    desc = fh.read()
+
+required = ["numpy", "scipy", "sklearn", "tensorflow", "deap"]
+
+setup(
+    name="simplemc",
+    version='0.0.1',
+    author='I Gomez-Vargas',
+    author_email="igomez@icf.unam.mx",
+    url="https://github.com/igomezv/neurapporx",
+    license="GPLv3",
+    description="Hyperparameter tuning for neural nets",
+    long_description=desc,
+    install_requires=required,
+    include_package_data=True,
+    keywords=["Hyperparameter",
+              "optimization",
+              "machine learning",
+              "deep learning",
+              "genetic algorithms"],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Natural Language :: English',
+    ],
+
+)
