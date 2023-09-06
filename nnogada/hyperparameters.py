9b163c406a6342ee3b74dc69d071861efb945837
Sarah Smith <neorelativista@gmail.com>
2023-09-06 10:43:31 -0600

    change return of best individual

--- nnogada/hyperparameters.py
+++ nnogada/hyperparameters.py
@@ -6,6 +6,6 @@ num_units = Hyperparameter(name='num_units', values=np.array([50, 100, 200]), va
 batch_size = Hyperparameter(name='batch_size', values=np.array([16, 32, 64]), val=16)
 learning_rate = Hyperparameter(name='learning_rate', values=np.array([1e-2, 1e-3, 1e-4]), val=1e-3)
 epochs = Hyperparameter(name='epochs', values=np.array([50, 100, 200]), val=100)
-act_fn = Hyperparameter(name='act_fn', values=['relu', 'sigmoid', 'tanh'], val='relu')
-last_act_fn = Hyperparameter(name='last_act_fn', values=['relu', 'sigmoid', 'linear'], val='linear')
+act_fn = Hyperparameter(name='act_fn', values=['relu', 'sigmoid', 'tanh'], val='tanh')
+last_act_fn = Hyperparameter(name='last_act_fn', values=['relu', 'sigmoid', 'linear'], val='sigmoid')
 loss_fn = Hyperparameter(name='loss_fn', values=['categorical_crossentropy', 'mean_squared_error'], val='mean_squared_error')
